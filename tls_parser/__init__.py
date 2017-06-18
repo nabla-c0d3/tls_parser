75bc663d2b466ef0658955c741763fce67b10650
Alban Diquet <nabla.c0d3@gmail.com>
2017-06-18 15:36:54 -0700

    Add link for reference

--- tls_parser/__init__.py
+++ tls_parser/__init__.py
@@ -5,4 +5,5 @@ __version__ = '1.0.0'
 __author__ = 'Alban Diquet'
 __email__ = 'nabla.c0d3@gmail.com'
 
-
+# A very useful page for working on this module:
+# http://blog.fourthbit.com/2014/12/23/traffic-analysis-of-an-ssl-slash-tls-session
