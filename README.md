02166c66ae340e45aeb9c6328b66df9bb146fa7e
Alban Diquet <nabla.c0d3@gmail.com>
2017-07-22 15:16:45 -0700

    Prepare repo for PyPi release

---
+++ README.md
@@ -0,0 +1,7 @@
+tls_parser
+==========
+
+[![Build Status](https://travis-ci.org/nabla-c0d3/tls_parser.svg?branch=master)](https://travis-ci.org/nabla-c0d3/tls_parser)
+[![PyPI version](https://badge.fury.io/py/tls_parser.svg)](https://badge.fury.io/py/tls_parser)
+
+Small library to parse TLS records; used by [SSLyze](https://github.com/nabla-c0d3/sslyze).
--- setup.py
+++ setup.py
@@ -8,14 +8,14 @@ from tls_parser import __email__
 TLS_PARSER_SETUP = {
     'name': 'tls_parser',
     'version': __version__,
-    'description': 'TLS handshake parser.',
+    'description': 'Small library to parse TLS records.',
     'author': __author__,
     'author_email': __email__,
+    'url': 'https://github.com/nabla-c0d3/tls_parser',
     'packages': ['tls_parser'],
     'extras_require': {':python_version < "3.4"': ['enum34'],
                        ':python_version < "3.5"': ['typing']},
 }
-
 
 if __name__ == "__main__":
     setup(**TLS_PARSER_SETUP)
