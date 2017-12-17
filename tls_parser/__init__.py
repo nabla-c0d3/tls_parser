5c77d2a4566940706613a6b31d50245dfe8307b5
Alban Diquet <nabla.c0d3@gmail.com>
2017-12-16 18:26:48 -0800

    Bump version number

--- tls_parser/__init__.py
+++ tls_parser/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from __future__ import print_function
 
-__version__ = '1.1.1'
+__version__ = '1.2.0'
 __author__ = 'Alban Diquet'
 __email__ = 'nabla.c0d3@gmail.com'
 
