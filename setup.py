78e586d2a6e1ebecbb44397c5f901e4d2d01d58c
Alban Diquet <nabla.c0d3@gmail.com>
2020-06-20 15:25:23 -0700

    Improve setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,13 @@ from tls_parser import __version__
 from tls_parser import __author__
 from tls_parser import __email__
 
-TLS_PARSER_SETUP = {
-    'name': 'tls_parser',
-    'version': __version__,
-    'description': 'Small library to parse TLS records.',
-    'author': __author__,
-    'author_email': __email__,
-    'url': 'https://github.com/nabla-c0d3/tls_parser',
-    'packages': ['tls_parser'],
-}
-
-if __name__ == "__main__":
-    setup(**TLS_PARSER_SETUP)
+setup(
+    name="tls_parser",
+    version=__version__,
+    description="Small library to parse TLS records.",
+    author=__author__,
+    author_email=__email__,
+    url="https://github.com/nabla-c0d3/tls_parser",
+    packages=["tls_parser"],
+    python_requires=">=3.7",
+)
