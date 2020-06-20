2ff95e8fb096ce7a7c0e81e03a4743da81097e59
Alban Diquet <nabla.c0d3@gmail.com>
2020-06-20 10:20:34 -0700

    Modernize test suite

--- tests/test_alert_protocol.py
+++ tests/test_alert_protocol.py
@@ -1,23 +1,21 @@
-from __future__ import absolute_import
-from __future__ import print_function
+import pytest
 
-import unittest
 from tls_parser.alert_protocol import TlsAlertRecord, TlsAlertSeverityByte
 from tls_parser.exceptions import UnknownTlsVersionByte
 
 
-class TlsAlertRecordTestCase(unittest.TestCase):
+class TestTlsAlertRecord:
 
     def test_from_bytes(self):
         alert_bytes = b'\x15\x03\x03\x00\x02\x02\x14'
         parsed_record, len_consumed = TlsAlertRecord.from_bytes(alert_bytes)
-        self.assertEqual(parsed_record.alert_severity, TlsAlertSeverityByte.FATAL)
-        self.assertEqual(parsed_record.alert_description, 0x14)
-        self.assertEqual(len_consumed, len(alert_bytes))
+        assert parsed_record.alert_severity == TlsAlertSeverityByte.FATAL
+        assert parsed_record.alert_description == 0x14
+        assert len_consumed == len(alert_bytes)
 
     def test_from_bytes_with_invalid_version(self):
         # Related to https://github.com/nabla-c0d3/sslyze/issues/437
         # Some servers put invalid TLS version bytes in the TLS alert they send back
         alert_bytes_with_bad_version = b'\x15\x00\x00\x00\x02\x02('
-        with self.assertRaises(UnknownTlsVersionByte):
+        with pytest.raises(UnknownTlsVersionByte):
             TlsAlertRecord.from_bytes(alert_bytes_with_bad_version)
--- tests/test_heartbeat_protocol.py
+++ tests/test_heartbeat_protocol.py
@@ -1,14 +1,9 @@
-from __future__ import absolute_import
-from __future__ import print_function
-
-import unittest
-
 from tls_parser.heartbeat_protocol import TlsHeartbeatRequestRecord
 from tls_parser.tls_version import TlsVersionEnum
 
 
-class TlsHeartbeatRequestRecordTestCase(unittest.TestCase):
+class TestTlsHeartbeatRequestRecord:
 
     def test_to_bytes(self):
         record = TlsHeartbeatRequestRecord.from_parameters(TlsVersionEnum.TLSV1_2, b'123456')
-        self.assertTrue(record.to_bytes())
+        assert record.to_bytes()
